a2de40cc7b26acd88ad57ac5553bd15348c8c0d7
Daniel Stevens <dan.stevens@oracle.com>
2013-01-29 15:27:59 +0000

    Modified ActiveWindowWatcher.cs: Changed code doc for ActiveWindowWatcher class.

--- ActiveWindowWatcher.cs
+++ ActiveWindowWatcher.cs
@@ -6,8 +6,13 @@ using System.ComponentModel;
 namespace System.Diagnostics
 {
 
-    /// <summary>Provides access to a <see cref="Process"/> object representing the currently active window
-    /// (system-wide).</summary>
+    /// <summary>Provides access to currently active window and triggers an event when it changes. This applies to all 
+    /// application running on the system, not just the host application.</summary>
+    /// <remarks>To use the ActiveWindowWatcher component, add the ActiveWindowWatcher.cs to your project. The 
+    /// component will be available in the Form Designer and can added to a form or other control. The Enabled 
+    /// property is set to false by default. Set this to true at design-time or run-time and the component will 
+    /// automatically respond to the appropriate Windows events and update the respective properties to correspond 
+    /// with the currently active window and will call the Changed event (if EnableRaisingEvents is true).</remarks> 
     [System.ComponentModel.DesignerCategory("")]
     public class ActiveWindowWatcher : Component
     {
